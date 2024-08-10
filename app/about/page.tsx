f2b854e4067df4df68a44d2bd35841607fc906ed
KHUSHI 444 <khankhushi4452@gmail.com>
2024-08-10 14:49:28 +0530

    basic setup

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>This is an about page...</div>
+  )
+}
+
+export default page
---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className='bg-blue-400'>
+      <div className='flex items-center justify-between w-[80%] mx-auto h-[50px]'>
+        <h1>Logo</h1>
+        <nav>
+          <ul className='flex items-center gap-3'>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link href="/signin">Sign In</Link>
+            </li>
+          </ul>
+        </nav>
+      </div> 
+    </header>
+  );
+}
